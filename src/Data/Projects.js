08efce72cfb3484f400cda6620eac8306bfe8636
Unk171 <7353687@gmail.com>
2023-10-25 11:43:54 -0400

    fixed bugs

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -1,43 +1,45 @@
+import {screenshot1, screenshot11, screenshot14, screenshot3, screenshot5, screenshot6} from './images'
+
 export const projects = [
   {
     id: 1,
     name: "Notepad",
-    image: "/src/components/images/screenshot11.png",
+    image: screenshot11,
     link: "https://github.com/Unk171/NotePad",
     depLink: "https://polar-forest-89089-257cc9f894ba.herokuapp.com",
   },
   {
     id: 2,
     name: "Blog",
-    image: "/src/components/images/screenshot14.png",
+    image: screenshot14,
     link: "https://github.com/Unk171/Blog",
     depLink: "https://floating-gorge-35639-a29f57abc72e.herokuapp.com/",
   },
   {
     id: 3,
     name: "Weather",
-    image: "/src/components/images/screenshot6.png",
+    image: screenshot6,
     link: "https://github.com/Unk171/Weather",
     depLink: "https://unk171.github.io/Weather/",
   },
   {
     id: 4,
     name: "Quiz",
-    image: "/src/components/images/screenshot1.png",
+    image: screenshot1,
     link: "https://github.com/Unk171/Quiz",
     depLink: "https://unk171.github.io/Quiz/",
   },
   {
     id: 5,
     name: "Work-scheduler",
-    image: "/src/components/images/screenshot5.png",
+    image: screenshot5,
     link: "https://github.com/Unk171/Work-scheduler",
     depLink: "https://unk171.github.io/Work-scheduler/",
   },
   {
     id: 6,
     name: "Passgen",
-    image: "/src/components/images/screenshot3.png",
+    image: screenshot3,
     link: "https://github.com/Unk171/Passgen",
     depLink: "https://unk171.github.io/Passgen/",
   },
---
+++ src/Data/images.js
@@ -0,0 +1,8 @@
+import screenshot11 from '../components/images/screenshot11.png'
+import screenshot14 from '../components/images/screenshot14.png'
+import screenshot6 from '../components/images/screenshot6.png'
+import screenshot1 from '../components/images/screenshot1.png'
+import screenshot5 from '../components/images/screenshot5.png'
+import screenshot3 from '../components/images/screenshot3.png'
+
+export {screenshot1, screenshot11, screenshot14, screenshot3, screenshot5, screenshot6}
